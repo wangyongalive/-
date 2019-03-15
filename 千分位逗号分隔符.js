41e0c9a4862f12a0e7b1707d7e7c0ebafac60011
wangyongaliveuser.email <jsonwang0102@163.com>
2019-03-15 21:57:00 +0800

    ''

---
+++ 千分位逗号分隔符.js
@@ -0,0 +1,15 @@
+// let str = '123456';
+// let reg = /(?=(\B\d{3})+$)/g;
+// console.log(str.replace(reg, ','));
+//
+//
+//
+//
+//
+
+let arr1 = "ping".split("");
+let arr2 = arr1.reverse(); // 会改变原数据
+let arr3 = 'pw'.split("");
+console.log(arr2.push(arr3));
+console.log(arr1);
+console.log(arr2);
