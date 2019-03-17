5c4cdac1bbdb2c17ac25c5834d20c3a43ce5c7d7
wangyongaliveuser.email <jsonwang0102@163.com>
2019-03-17 12:53:52 +0800

    '数组去重'

---
+++ 数组去重.js
@@ -0,0 +1,14 @@
+const unique = (arr, result = []) => {
+    const obj = {};
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        if (!obj[arr[i]]) { // 键没有，则添加
+            obj[arr[i]] = true;
+            result.push(arr[i]);
+        } else if (!result.includes(arr[i])) { // 如果没有值 则添加
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
+console.log(unique([1, 2, 2, 3, '1', 5, 4, 3, 2]));
