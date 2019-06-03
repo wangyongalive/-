ae3bb153bf99659eed0d9df9f6d61aaed9786b62
wangyongaliveuser.email <jsonwang0102@163.com>
2019-06-03 10:00:17 +0800

    ''

--- 算法与数据结构/递归/斐波那契数.js
+++ 算法与数据结构/递归/斐波那契数.js
@@ -1,7 +1,7 @@
 function fibonacciMemozation(n) {
     const memo = [0, 1];
     const fibonacci = (n) => {
-        if (memo[n] != null) return memo[n];
+        if (memo[n] != null) return memo[n]; // 缓存
         return memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
     };
     return fibonacci(n);
