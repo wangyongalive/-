7441db1a083b039f26fb9f34629c4fc20a9e9321
wangyongaliveuser.email <jsonwang0102@163.com>
2019-06-03 20:41:53 +0800

    ''

---
+++ 算法与数据结构/分治法/二分搜索-分治法.js
@@ -0,0 +1,26 @@
+import {Compare, defaultCompare, DOES_NOT_EXIST} from '../util.js';
+
+function binarySearchRecursive(array, value, low, high, compareFn = defaultCompare) {
+    if (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+        const element = array[mid];
+        if (compareFn(element, value) === Compare.lessThan) {
+            return binarySearchRecursive(array, value, mid + 1, high, compareFn);
+        }
+        if (compareFn(element, value) === Compare.moreThan) {
+            return binarySearchRecursive(array, value, low, mid - 1, compareFn);
+        }
+        return mid;
+    }
+    return DOES_NOT_EXIST;
+}
+
+export function binarySearch(array, value, compareFn = defaultCompare) {
+    const low = 0;
+    const high = array.length - 1;
+    return binarySearchRecursive(array, value, low, high, compareFn);
+}
+
+
+let arr = [1, 2, 3, 4, 5, 6]
+console.log(binarySearch(arr, 5));
--- 算法与数据结构/排序和搜索/归并排序.js
+++ 算法与数据结构/排序和搜索/归并排序.js
@@ -15,7 +15,7 @@ function mergeSort(items) {
     if (items.length == 1) {
         return items;
     }
-    var middle = Math.floor(items.length / 2),
+    let middle = Math.floor(items.length / 2),
         left = items.slice(0, middle),
         right = items.slice(middle);
     return merge(mergeSort(left), mergeSort(right));
